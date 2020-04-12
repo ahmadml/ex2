6685285c74cc027a280fccac1bfb1743ad991c4e
ahmadml <57864630+ahmadml@users.noreply.github.com>
2020-04-12 17:01:55 +0300

    Add files via upload

---
+++ FamilyTree.cpp
@@ -0,0 +1,27 @@
+#include"FamilyTree.hpp"
+using namespace family;
+
+
+Tree Tree::addFather(string person, string name){
+    
+}
+
+Tree Tree::addMother(string person, string name){
+    
+}
+
+string Tree::relation(string name){
+    return name;
+}
+
+string Tree::find(string name){
+    return name;
+}
+
+void Tree::display(){
+    
+}
+
+void Tree::remove(string name){
+
+}
